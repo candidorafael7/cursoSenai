7ca626fbefd2f21905d2d3c269cdbc1d84860268
candidorafael7 <144627067+candidorafael7@users.noreply.github.com>
2023-11-11 21:45:35 -0300

    Add files via upload

---
+++ src/components/footer.jsx
@@ -0,0 +1,15 @@
+import './css/footer.css'
+
+
+function Footer() {
+
+  return (
+    <>
+      <footer>
+        <h3>Feito no curso de Front-end 2023</h3>
+        </footer>
+    </>
+  )
+}
+
+export default Footer
